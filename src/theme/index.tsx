25bcc9e61ac27dd10d794bb8f76b9dad4d660b44
GustaJLemos <gustavolemosmendes@gmail.com>
2022-11-27 22:23:05 -0300

    Criando nosso arquivo de tema

---
+++ src/theme/index.tsx
@@ -0,0 +1,39 @@
+export const theme = {
+  COLORS: {
+    base: {
+      gray100: '#1B1D1E',
+      gray200: '#333638',
+      gray300: '#5C6265',
+      gray400: '#B9BBBC',
+      gray500: '#DDDEDF',
+      gray600: '#EFF0F0',
+      gray700: '#FAFAFA',
+      white: '#FFFFFF',
+    },
+    brand: {
+      green: {
+        dark: '#639339',
+        mid: '#DCF5C4',
+        light: '#E5F0DB',
+      },
+      red: {
+        dark: '#BF3B44',
+        mid: '#F3BABD',
+        light: '#F4E6E7',
+      }
+    }
+  },
+  FONT_SIZE: {
+    title_G: 32 + 'px',
+    title_M: 24 + 'px',
+    title_S: 18 + 'px',
+    title_XS: 14 + 'px',
+    body_M: 16 + 'px',
+    body_S: 14 + 'px',
+    body_XS: 12 + 'px',
+  },
+  FONT_FAMILY: {
+    regular: 'NunitoSans_400Regular',
+    bold: 'NunitoSans_700Bold',
+  }
+}
