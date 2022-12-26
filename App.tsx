81272356c9f7ed4813ae0602b716fa41dc07114f
GustaJLemos <gustavolemosmendes@gmail.com>
2022-12-26 20:09:11 -0300

    Adicionando as rotas

--- App.tsx
+++ App.tsx
@@ -1,16 +1,18 @@
 import { Home } from '@screens/Home';
 import { theme } from '@theme/index';
-import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components';
 import { useFonts, NunitoSans_400Regular, NunitoSans_700Bold } from '@expo-google-fonts/nunito-sans'
 import { Loading } from '@components/Loading';
+import { Estatistics } from '@screens/Estatistics';
+import { NewMeal } from '@screens/NewMeal';
+import { Routes } from './src/routes/index';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ NunitoSans_400Regular, NunitoSans_700Bold }) 
 
   return (
     <ThemeProvider theme={theme}>
-      {fontsLoaded ? <Home /> : <Loading />}
+        {fontsLoaded ? <Routes /> : <Loading />}
     </ThemeProvider>
   );
 }
